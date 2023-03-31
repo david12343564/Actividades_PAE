401c9844f59a473a5b8bba0df273a2c528b0f25a
David <david_emmi@hotmail.com>
2023-03-31 00:02:22 -0600

    Creación de componentes
    Se crea la estructura de carpetas necesaria para los componentes y se crean aquellos que van a ser necesarios

--- tarea-6/src/app/app.module.ts
+++ tarea-6/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { EducacionComponent } from './pages/educacion/educacion.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    InicioComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
