8ee7ffa3f572a4d6243d20088c40be6cb71da561
David <david_emmi@hotmail.com>
2023-03-31 21:00:54 -0600

    Tarea 6 terminada
    Se completaron todas las páginas de la tarea, html y estilos

--- tarea-6/src/app/app-routing.module.ts
+++ tarea-6/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { EducacionComponent } from './pages/educacion/educacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:InicioComponent},
+  {path: 'contacto', component:ContactoComponent},
+  {path: 'cursos', component:CursosComponent},
+  {path: 'proyectos', component:ProyectosComponent},
+  {path: 'educacion', component:EducacionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
